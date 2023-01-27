d8eb24eed2ab6381c878ddedd9431074a570f8cb
Thomas T <tranthom618@gmail.com>
2023-01-26 20:16:18 -0500

    Date + Time added to header (I included time to track each timeblock's completion. Will see later if necessary)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var timeDisplayEl = $('#currentDay');
+
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+displayTime();
+setInterval(displayTime, 1000);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +31,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  
 });
