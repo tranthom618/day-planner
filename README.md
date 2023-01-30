ac19b92179682f550f859ae4b8e5f148964a8b12
Thomas T <tranthom618@gmail.com>
2023-01-29 21:04:31 -0500

    Figured out how to allow each timeblock to save text locally

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # day-planner
+
+## Description
+
+A work day planner that will demonstrate my ability to implement a Javascript library like dayjs. This project will also expand upon my understanding of Javascript user input, local storage, and organization.
+
+## Installation
+
+For the purposes of this project, the web application will be hosted on GitHub Pages (https://tranthom618.github.io/day-planner/)
+
+
+## Usage
+
+![Preview of navigation links](assets/images/readme-instructional.png?raw=true "README Example")
+
+Each timeblock for a typical 9 am to 5 pm work day will dynamically update with colour coding to indicate between past (grey), present (red), and future (green). The user can click into each timeblock to add an event by text. To the right there blue floppy disk icon that is a button, which will the save the event details. The page will save the events to the local browser storage to provide persistent storage when the page is closed or refreshed.
+
+## Credits
+
+reset.css template sourced from http://meyerweb.com/eric/tools/css/reset/ --- v2.0 | 20110126 --- License: none (public domain)
+
+## License
+
+MIT License - See LICENSE file contained within directory.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
 // jQuery shorthand to set the function to await being called until after the DOM fully loads
 $(function () {
 
-    // Time display in header with date. Time will be used to keep planner colour coordination up to date.
-    const timeDisplayEl = $('#currentDay');
-
-    function displayTime() {
-      var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-      timeDisplayEl.text(rightNow);
-    }
-
-    // Calls the display time function immediately to load with an interval at 1 second of reloading.
-    displayTime();
-    setInterval(displayTime, 1000);
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Individual buttons for each timeblock's texct input and save button. Should be easier to organize instead of running for loops and verifying selection.
+  var am9BtnEl = document.querySelector("#am9Btn");
+  var am9EventEl = document.getElementById("#am9Event");
   
+  var am10BtnEl = document.querySelector("#am10Btn");
+  var am10EventEl = document.querySelector("#am10Event");
+
+  var am11BtnEl = document.querySelector("#am11Btn");
+  var am11EventEl = document.querySelector("#am11Event");
+
+  var pm12BtnEl = document.querySelector("#pm12Btn");
+  var pm12EventEl = document.querySelector("#pm12Event");
+
+  var pm1BtnEl = document.querySelector("#pm1Btn");
+  var pm1EventEl = document.querySelector("#pm1Event");
+
+  var pm2BtnEl = document.querySelector("#pm2Btn");
+  var pm2EventEl = document.querySelector("#pm2Event");
+
+  var pm3BtnEl = document.querySelector("#pm3Btn");
+  var pm3EventEl = document.querySelector("#pm3Event");
+
+  var pm4BtnEl = document.querySelector("#pm4Btn");
+  var pm4EventEl = document.querySelector("#pm4Event");
+
+  var pm5BtnEl = document.querySelector("#pm5Btn");
+  var pm5EventEl = document.querySelector("#pm5Event");
+    
+
+
+  // Time display in header with date. Time will be used to keep planner colour coordination up to date.
+  const timeDisplayEl = $('#currentDay');
+
+  // Allows the timetableContainer element to be selected for use under pastPresFuture function
   var timetableEl = document.querySelector("#timetableContainer");
+
+
+
+
+
+  // Clock and Date function, allows it to be periodically updated every 1000 ms
+  function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+  // Calls the display time function immediately to load with an interval at 1 second of reloading.
+  displayTime();
+  setInterval(displayTime, 1000);
   
+
+
+  // Clock checker for comparing current hour to each timeblock and colour coding by past/present/future
   function pastPresFuture() {
     
     // dayjs format to retrieve just the hour in 24 hour format
@@ -45,36 +71,23 @@ $(function () {
           timetableEl.children[i].className = "row time-block present";
         }
 
-        // By the time the currhour reaches 1 pm, 'i' will be 5, so to compare them, i has to be subtracted to check if the timeblock is in the present.
+        // else if statement to check if the timeblock is in the future
         else if (i+9 > currHour) {
           timetableEl.children[i].className = "row time-block future";
         }
 
-
+        // else if statement to check if the timeblock is in the future
         else if (i+9 < currHour) {
           timetableEl.children[i].className = "row time-block past";
         }
       }
     }
 
-    // "row time-block present"
-    // "row time-block past"
+    // Calls for the pastPresFuture function to be run once DOM is loaded
     pastPresFuture();
-  }
-  
-  
-  
-
-  // <div id="hour-9" class="row time-block past">
-  //       <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-  //       <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-  //       <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  //         <i class="fas fa-save" aria-hidden="true"></i>
-  //       </button>
-  //     </div>
 
 
-  // TODO: Add a listener for click events on the save button. This code should
+    // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -84,10 +97,63 @@ $(function () {
 
 
 
+
+
+  ////////// 9 am Input Save and Button Listener //////////
+  // Retrieves the locally saved value of the 9 am events
+  am9SavedEvent = JSON.parse(localStorage.getItem("am9Event"));
+  document.getElementById("am9Event").innerHTML = am9SavedEvent;
+
+  // Event Listener for the "Save" button and ties it to input text field for the event
+  am9BtnEl.addEventListener("click", function () {
+
+    // Saves the current input value of the timeblock and saves it in a variable
+    var savedEvent = document.getElementById("am9Event").value;
+
+    // Saves the text input to local storage
+    localStorage.setItem("am9Event", JSON.stringify(savedEvent));
+    console.log(JSON.parse(localStorage.getItem("am9Event")));
+  });
+
+  ////////// 10 am Input Save and Button Listener //////////
+  // Retrieves the locally saved value of the 10 am events
+  am10SavedEvent = JSON.parse(localStorage.getItem("am10Event"));
+  document.getElementById("am10Event").innerHTML = am10SavedEvent;
+  
+  // Event Listener for the "Save" button and ties it to input text field for the event
+  am10BtnEl.addEventListener("click", function () {
+  
+    // Saves the current input value of the timeblock and saves it in a variable
+    var savedEvent = document.getElementById("am10Event").value;
+  
+    // Saves the text input to local storage
+    localStorage.setItem("am10Event", JSON.stringify(savedEvent));
+    console.log(JSON.parse(localStorage.getItem("am10Event")));
+    });
+  
+  ////////// 11 am Input Save and Button Listener //////////
+  // Retrieves the locally saved value of the 11 am events
+  am11SavedEvent = JSON.parse(localStorage.getItem("am11Event"));
+  document.getElementById("am11Event").innerHTML = am11SavedEvent;
+
+  // Event Listener for the "Save" button and ties it to input text field for the event
+  am11BtnEl.addEventListener("click", function () {
+
+    // Saves the current input value of the timeblock and saves it in a variable
+    var savedEvent = document.getElementById("am11Event").value;
+
+    // Saves the text input to local storage
+    localStorage.setItem("am11Event", JSON.stringify(savedEvent));
+    console.log(JSON.parse(localStorage.getItem("am11Event")));
+  });
+
+
+
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  
-);
+});
